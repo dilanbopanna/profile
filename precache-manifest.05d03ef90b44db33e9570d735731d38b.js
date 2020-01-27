c7ecf5598873ac25de7d3eb86b15f1c573287261
dilanbopanna <bopannamj@gmail.com>
2020-01-27 17:06:32 +0530

    Updates

---
+++ precache-manifest.05d03ef90b44db33e9570d735731d38b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b150b8d931d0e0bd282a1437912dda3c",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "95f60229b1642d24c926",
+    "url": "/profile/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "f3cebbacce2e96e2a372",
+    "url": "/profile/static/css/main.7ae4b6cf.chunk.css"
+  },
+  {
+    "revision": "95f60229b1642d24c926",
+    "url": "/profile/static/js/2.cfdfd433.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/profile/static/js/2.cfdfd433.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3cebbacce2e96e2a372",
+    "url": "/profile/static/js/main.66001c25.chunk.js"
+  },
+  {
+    "revision": "99fdb44f569243caeab7",
+    "url": "/profile/static/js/runtime-main.f3882d30.js"
+  },
+  {
+    "revision": "94961c6edaf24ccdaa4608d6e061c280",
+    "url": "/profile/static/media/MyCV.94961c6e.pdf"
+  }
+]);
